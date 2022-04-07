083d0247ffa098eaf23019bad5b0e6c6045e42b3
chandar <achandar74@gmail.com>
2022-04-07 11:09:23 +0530

    excel

---
+++ src/test/java/ExcelExample1.java
@@ -0,0 +1,39 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelExample1 {
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = new FileInputStream("C:\\Users\\JANARDHAN\\OneDrive\\Desktop\\Book.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		int sheets = workbook.getNumberOfSheets();
+		for (int i = 0; i < sheets; i++) {
+			if (workbook.getSheetName(i).equalsIgnoreCase("demo")) {
+				XSSFSheet sheet = workbook.getSheetAt(i);
+				Iterator<Row> row = sheet.rowIterator();
+				Row firstrow = row.next();
+				Iterator<Cell> ce = firstrow.cellIterator();
+				int k = 0;
+				int coulumn = 0;
+				while (ce.hasNext()) {
+					Cell r = ce.next();
+					if (r.getStringCellValue().equalsIgnoreCase("testscenario")) {
+						coulumn = k;
+					}
+					k++;
+				}
+				System.out.println(coulumn);
+				while(row.hasNext()) {
+					Row data = row.next();
+					System.out.println(data.getCell(coulumn).getStringCellValue());
+				}
+			}
+		}
+	}
+}
